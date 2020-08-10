0b1e56b98925469c6c2cb5c6e00d788c69426e9c
AtimaB <nickzehn10@hotmail.com>
2020-08-10 14:01:05 -0600

    first commit

---
+++ seed.sql
@@ -0,0 +1,18 @@
+-- USE employee_trackerDB;
+
+-- INSERT INTO department
+--     (id, department_name)
+-- VALUES
+--     (1, Sale) ,
+--     (2, Engineering),
+--     (3, Finance),
+--     (4, Legal),
+--     (5, Finance);
+
+-- INSERT INTO roles
+--     (id, title, salary, department_id)
+-- VALUES
+--     (1, Saleperson, 10000, 1),
+--     (2, Software
+-- Engineering, 30000, 2),
+    
